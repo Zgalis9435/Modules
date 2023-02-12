79d1d7ee98b611093171b1dc0d8ebc1d32f38427
Zgalis9435 <zharos.delgado@proton.me>
2023-02-12 18:24:05 +0100

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,15 @@
+class calculator():
+
+    def suma(a,b):
+        result=a+b
+        return result
+    def minus(a,b):
+        result=a-b
+        return result
+    def multi(a,b):
+        result=a*b
+        return result
+    def div(a,b):
+        result=a/b
+        return result
+
---
+++ main.py
@@ -0,0 +1,6 @@
+import calculator
+
+print(calculator.calculator.suma(5,24))
+print(calculator.calculator.div(25,5))
+print(calculator.calculator.multi(5,8))
+print(calculator.calculator.minus(18,7))
